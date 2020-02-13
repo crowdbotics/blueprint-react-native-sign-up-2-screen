14d01051c78b17e3219c94021c7424e0de086d0e
tanduong <tanduong@live.com>
2020-02-13 06:25:05 -0600

    PLAT-1223

--- SignUp02Blueprint/navigator.js
+++ SignUp02Blueprint/navigator.js
@@ -4,7 +4,7 @@ import {SignUp2Container} from './screens/signUp2/signUp2.container';
 
 import Home from './screens';
 
-export const SignUp02BlueprintNavigator = createStackNavigator(
+export default SignUp02BlueprintNavigator = createStackNavigator(
   {
     Home: {screen: Home},
     SignUp2: {screen: SignUp2Container},
